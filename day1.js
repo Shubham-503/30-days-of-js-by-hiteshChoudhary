07e8d82116ea30159ad60f8a9be1a8c6360690d5
Shubham-503 <kr.shubham025@gmail.com>
2024-07-14 12:43:49 +0530

    day 1 completed

---
+++ day1.js
@@ -0,0 +1,34 @@
+// Activity 1
+var temp1 = 4;
+console.log(temp1);
+let temp2 = "shubham";
+console.log(temp2);
+
+// Activity 2
+const temp3 = false;
+console.log(temp3);
+
+// Activity 3
+let numberVar = 1;
+let strvar = "Shubham";
+let boolvar = false;
+let obj = {
+  a: "temp",
+};
+let arr = [1, "shubham", true];
+console.log(typeof numberVar);
+console.log(typeof strvar);
+console.log(typeof boolvar);
+console.log(typeof obj);
+console.log(typeof arr);
+
+// Activity
+let temp4 = 23;
+console.log(temp4);
+temp4 = "newValue";
+console.log(temp4);
+// Activity 5
+const temp5 = 23;
+console.log(temp5);
+temp5 = "newValue";
+console.log(temp4);
